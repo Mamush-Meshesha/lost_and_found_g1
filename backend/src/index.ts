d3ec472e01a3a1b71e86fdcd22659c8f9ed4d760
Mamush-Meshesha <mamgithub@gmail.com>
2026-09-19 12:08:33 +0300

    report crud with image upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,22 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import itemRoutes from "./routes/itemRoutes.js";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
 
 // Middleware
 app.use(cors());
@@ -15,9 +25,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // Health check route
 app.get("/", (_req: Request, res: Response) => {
-  res.status(200).json({ success: true, message: "MERN backend is running" });
+  res.status(200).json({ success: true, message: "server  is running" });
 });
 
+app.use("/api/items", itemRoutes);
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running on http://localhost:${PORT}`);
 });
