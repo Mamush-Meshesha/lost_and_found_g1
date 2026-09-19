6c3522f6fa66c8efe273c36965aaaf15bf9deead
Mamush-Meshesha <mamgithub@gmail.com>
2026-09-19 13:49:48 +0300

    Fix: added auth middleware

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 export const connectDB = async (): Promise<void> => {
 try {
 await mongoose.connect(process.env.MONGO_URI!);
--- backend/src/routes/itemRoutes.ts
+++ backend/src/routes/itemRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middleware/uploadMiddleware.js";
+import  authMiddleware  from "../middleware/authMiddleware.js";
 
 import {
   createItem,
@@ -12,12 +13,12 @@ import {
 const router = Router();
 
 // Public
-router.get("/", getItems);
-router.get("/:id", getItemById);
+router.get("/", authMiddleware, getItems);
+router.get("/:id",authMiddleware, getItemById);
 
 // Authenticated
-router.post("/", upload.array("images", 5), createItem);
-router.patch("/:id", upload.array("images", 5), updateItem);
-router.delete("/:id", deleteItem);
+router.post("/", authMiddleware, upload.array("images", 5), createItem);
+router.patch("/:id",authMiddleware, upload.array("images", 5), updateItem);
+router.delete("/:id", authMiddleware, deleteItem);
 
 export default router;
